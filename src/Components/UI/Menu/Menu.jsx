2f75b63cbc2f57412c5f3f0da1cdc65e13ac65ce
rafal <raf.drozdowski@gmail.com>
2019-03-02 19:20:18 +0000

    add menu build from json and remove menu from oter folder

--- src/Components/UI/Menu/Menu.jsx
+++ src/Components/UI/Menu/Menu.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import MenuItem from './MenuItem.jsx';
 import JsonMenu from './Menu.json';
 
+
 export default class Menu extends Component {
-render(){
-  return (
-    <div>
-      <MenuItem />
-    </div>
-  )
-}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      this.JsonMenu = JsonMenu;
+    }
+    return (
+      <ul>
+        {
+          this.JsonMenu.map( item => {
+            return (
+            <MenuItem menu={item.menuTitle} />
+            )
+          })
+        }
+        </ul>
+    )
+  }
 }
--- src/Components/UI/Menu/MenuItem.jsx
+++ src/Components/UI/Menu/MenuItem.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import JsonMenu from './Menu.json';
 import './MenuItem.scss';
 
 export default class MenuItem extends Component {
   constructor(props) {
     super(props);
-    this.json = JsonMenu;
   }
   render() {
-
-    {
-      this.json.map( (i,k) => {
-        console.log(i);
-      })
-    }
-
     return (
-
+        <li>{this.props.menu}</li>
     )
   }
 }
