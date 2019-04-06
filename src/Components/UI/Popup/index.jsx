e0aafd852e76276a4097eb9d16e63408e7051b28
rafal <raf.drozdowski@gmail.com>
2019-04-06 14:41:18 +0100

    add gdpr popup

--- src/Components/UI/Popup/index.jsx
+++ src/Components/UI/Popup/index.jsx
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import Styles from './Styles.scss';
 
-
 class Popup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      consent: false
+    }
+  }
+
+  /*
+  * onClick action
+  */
+  consentClick() {
+
+    /**
+     * set state key
+     */
+    this.setState({
+      consent: true
+    })
+
+    /**
+     * set localStorage key 
+     */
+    window.localStorage.setItem(
+      'visited', true
+    )
+
+  }
+
   render() {
+    {
+      console.log(`Consent button ${this.state.consent}`);
+    }
+
     return (
       <div>
-        <div className={Styles.popup}>
-          <h4>Consent</h4>
-            <p></p>
+        { this.state.consent === false ? (
+        <div className={Styles.popup}>        
+          <h4>Compliance Check</h4>
+          <p>This site tracks visits anonymously using cookies. Close this dialog to confirm you are happy with that, or find out more in the best practice privacy statement.</p>
+          <button className={Styles.button} onClick={this.consentClick.bind(this)}>Agree and close</button>
         </div>
+        ) : (
+          ""
+        )}
       </div>
     )
   }
