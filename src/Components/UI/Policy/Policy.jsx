90c53b56c98c6787c003cd1bf0c39dfaf4681cd0
rafal <raf.drozdowski@gmail.com>
2019-04-08 19:41:47 +0100

    add policy partial

---
+++ src/Components/UI/Policy/Policy.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Styles from './Policy.scss';
+
+class Policy extends React {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className={Styles.Policy}>
+        <p>This is come text</p>
+        <p>This is come text</p>
+        <p>This is come text</p>
+        <p>This is come text</p>
+        <p>This is come text</p>
+        <p>This is come text</p>
+      </div>
+  )
+  }
+  default extends Policy
+}
