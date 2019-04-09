9d28f419ab20af2ecd43620e2e33af34e8182bab
rafal <raf.drozdowski@gmail.com>
2019-04-09 21:11:33 +0100

    add google analytics

--- src/Components/UI/Popup/Popup.jsx
+++ src/Components/UI/Popup/Popup.jsx
@@ -30,6 +30,20 @@ class Popup extends Component {
       'visited', true
     )
 
+      /**
+       * add google analytics script 
+       */
+
+       const scpt = document.createElement('script');
+       let googleScript = (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                            ga('create', 'UA-26045403-1', 'auto');
+                            ga('send', 'pageview');
+
+
+
   }
 
   render() {
